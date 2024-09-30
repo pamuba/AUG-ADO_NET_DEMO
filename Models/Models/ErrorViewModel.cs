80dc8800800bffaa14ec3003f0c750c9e85f2bcc
Partha Pratim Bora <me.in@live.in>
2024-09-30 19:26:39 +0530

    30Sep_Starting

---
+++ Models/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFCROEDEMOS.EF_Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
